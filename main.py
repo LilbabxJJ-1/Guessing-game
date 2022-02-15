aaeecd57ce68361404fa52bb225a67cbbadf9b4a
JJ <99676249+LilbabxJJ-1@users.noreply.github.com>
2022-02-15 16:36:03 +0000

    update

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import random
 
 #variables
-print("You have to guess a number between 1 and 15! Each turn, you'll get a hint. You have 5 chances")
+print("You have to guess a number between 1 and 15! Each turn, you'll get a hint to go higher or lower. You have 10 chances")
 number = random.randint(1,15)
 print(number)
 nums = []
 nums.append(number)
-hint = 0
-turnsl = 5
+turnsl = 10
 turnst = 0
 
 
@@ -16,20 +15,38 @@ while True:
  try:
   guess_number = int(input("\nWhat's your guess? "))
   if guess_number not in nums and turnsl != 1:
-   hint += 1
    turnsl -= 1
    turnst += 1
    print(f"That is incorrect! {turnsl} more tries!")
+   with open("guesses.txt", "a") as guesses:
+     guesses.write(f"Try {turnst}: {guess_number} \n")
+   guesses.close
+
    if guess_number < number:
      print("Aim Higher!")
    else:
      print("Aim Lower!")  
+  
 
-  elif turnsl == 1:
-    print(f"You failed! The number was {number}!")
+  elif turnsl == 1 and number != guess_number:
+    
+  
+    print(f"\nYou failed!")
+    print("Summary")
+
+    with open("guesses.txt", "r") as guesses:
+     print(guesses.read())
+    guesses.close()
+    print(f"Correct Number: {number}")
+    
+    with open("guesses.txt", "w") as guesses:
+      guesses.write("")
+    guesses.close()
     break
+  
+  #error and correct
   else:
-   print(f"You are correct! You guessed after {turnst} tries")
+   print(f"You are correct! You guessed after {turnst} tries.")
    break
  except ValueError:
     print("That's not a number!")
